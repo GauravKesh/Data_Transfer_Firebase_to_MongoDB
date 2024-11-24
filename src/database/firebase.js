957ba493ad85c8292f7bd10b343c79f5734c0bed
RohitM1518 <ROHIT.20211CSE0674@presidencyuniversity.in>
2024-11-24 12:25:27 +0530

    Update Firebase configuration to use environment variables

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -2,12 +2,12 @@ const { initializeApp } = require("firebase/app");
 const { getFirestore } = require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0HJ7OhRTXuuGk_d4yWRfSC2TcczoT7Nc",
-  authDomain: "blogsdata-246d6.firebaseapp.com",
-  projectId: "blogsdata-246d6",
-  storageBucket: "blogsdata-246d6.firebasestorage.app",
-  messagingSenderId: "304745682537",
-  appId: "1:304745682537:web:343f768e346aa707f634f8",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
